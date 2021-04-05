a49187ab9eda559b0b79918a62e2ceed78db44f3
Warthog710 <computergeek710@gmail.com>
2021-04-04 19:23:12 -0700

    Implemented chunk based rendering/collision
    Only the visible obstacles are drawn. Also, only the obstacles in nearby chunks are checked for collisions. This greatly improves performance when 100s or 1000s of obstacles exist.

--- obstacles.py
+++ obstacles.py
@@ -1,17 +1,19 @@
 import pygame
+import math
 import constants
+import random
 
 class obstacles:
     def __init__(self, plyr):
         #Textures
-        self.__sawBladeTexture = pygame.image.load('./assets/obstacles/sawbladeScaled.png').convert()
+        self.__sawBladeTexture = pygame.image.load('./assets/obstacles/saw.png').convert()
         self.__vineTexture = pygame.image.load('./assets/obstacles/vine.png').convert()
         self.__boxTexture = pygame.image.load('./assets/obstacles/box.png').convert()
         self.__coinTexture = pygame.image.load('./assets/obstacles/coin.png').convert()
         self.__boxTexture.set_colorkey(constants.COLOR_WHITE)
         self.__coinTexture.set_colorkey(constants.COLOR_WHITE)
-        self.__vineTexture.set_colorkey(constants.COLOR_BLACK)
-        self.__sawBladeTexture.set_colorkey(constants.COLOR_WHITE)
+        self.__vineTexture.set_colorkey(constants.COLOR_WHITE)
+        self.__sawBladeTexture.set_colorkey(constants.COLOR_BLACK)
 
         #Scale textures
         sawBladeSize = self.__sawBladeTexture.get_size()
@@ -23,44 +25,75 @@ class obstacles:
         self.__boxTexture = pygame.transform.scale(self.__boxTexture, (int(boxSize[0] * constants.OBSTACLES_SCALE), int(boxSize[0] * constants.OBSTACLES_SCALE)))
         self.__coinTexture = pygame.transform.scale(self.__coinTexture, (int(coinSize[0]), int(coinSize[0])))
 
-
-        self.__obstaclesList = [('saw', (200, 0)), ('vine', (600, 0)), ('box', (800, 0)), ('coin', (1000, -10), True), ('coin', (1040, -50), True), ('coin', (1080, -90), True)]
+        #? Idea: the screen is 1280px wide. Lets split the screen into 16chunks of 80 pixels. We will then place 1 or none obstacles in these chunks.
+        #? During collision detection, just detect collisions in the chunks behind or in front
+        #? For drawing, only draw the visible chunks
+        #? Note: Current chunk = self.__startingPlayerChunk + math.floor(self.__plyr.distance / self.__chunkWidth)
 
         #Save player reference
         self.__plyr = plyr
 
+        #Chunk info
+        self.__chunkWidth = constants.SCREEN_WIDTH / constants.NUM_CHUNKS
+        self.__startingPlayerChunk = math.floor(self.__plyr.pos[0] / self.__chunkWidth)
+
+        self.__obstacleDict = {
+            3: ('saw', (0, 0)),
+            7: ('vine', (0, 0)),
+            10: ('box', (0, 0)),
+            11: ('box', (0, -96)),
+            12: ('box', (0, 0)),
+            14: ('coin', (0, 0), True),
+            15: ('coin', (0, -40), True),
+            16: ('coin', (0, -80), True),
+        }
+
+    #? For collisions, we will look at the the players chunk and before and ahead of it 2 chunks
     def detectCollision(self):
+        currentChunk = self.__startingPlayerChunk + math.floor(self.__plyr.distance / self.__chunkWidth)
+        startingChunk = currentChunk - 2
 
-        for x in range(0, len(self.__obstaclesList)):
-            if 'saw' in self.__obstaclesList[x][0]:
-                obstacleRect = self.getSawBladeRect(self.__obstaclesList[x][1])
-                
-                if obstacleRect.colliderect(self.__plyr.getBoundingBox()):
-                    return True
-            
-            elif 'vine' in self.__obstaclesList[x][0]:
-                obstacleRect = self.getVineRect(self.__obstaclesList[x][1])
-                
-                if obstacleRect.colliderect(self.__plyr.getBoundingBox()):
-                    return True
+        for startingChunk in range(startingChunk, currentChunk + 2):
+            #If an object exists in that chunk, perform collision detection
+            if startingChunk in self.__obstacleDict:
+                #Check object type
+                if 'saw' in self.__obstacleDict[startingChunk][0]:
+                    pos = (self.__transChunkToPxl(startingChunk) + self.__obstacleDict[startingChunk][1][0], self.__obstacleDict[startingChunk][1][1])
+                    obstacleRect = self.getSawBladeRect(pos)
 
-            elif 'box' in self.__obstaclesList[x][0]:
-                obstacleRect = self.getBoxRect(self.__obstaclesList[x][1])
+                    #Check if the player collides
+                    if obstacleRect.colliderect(self.__plyr.getBoundingBox()):
+                        #? Since we hit a saw, we will reset the game
+                        self.__plyr.reset()
 
-                if obstacleRect.colliderect(self.__plyr.getBoundingBox()):
-                    return True 
+                        #! Don't forget to reset the coins!!!
+                        return True
+                elif 'box' in self.__obstacleDict[startingChunk][0]:
+                    pos = (self.__transChunkToPxl(startingChunk) + self.__obstacleDict[startingChunk][1][0], self.__obstacleDict[startingChunk][1][1])
+                    obstacleRect = self.getBoxRect(pos)
 
-            elif 'coin' in self.__obstaclesList[x][0] and self.__obstaclesList[x][2]:
-                obstacleRect = self.getCoinRect(self.__obstaclesList[x][1])
+                    #Check if the player collides
+                    if obstacleRect.colliderect(self.__plyr.getBoundingBox()):
+                        return True
+                elif 'vine' in self.__obstacleDict[startingChunk][0]:
+                    pos = (self.__transChunkToPxl(startingChunk) + self.__obstacleDict[startingChunk][1][0], self.__obstacleDict[startingChunk][1][1])
+                    obstacleRect = self.getVineRect(pos)
 
-                #If we collide, record it, add it to coins collected and remove it from the map
-                if obstacleRect.colliderect(self.__plyr.getBoundingBox()):
-                    self.__plyr.coinsCollected += 1
-                    self.__obstaclesList[x] = (self.__obstaclesList[x][0], self.__obstaclesList[x][1], False)
+                    #Check if the player collides
+                    if obstacleRect.colliderect(self.__plyr.getBoundingBox()):
+                        return True
+                elif 'coin' in self.__obstacleDict[startingChunk][0] and self.__obstacleDict[startingChunk][2]:
+                    pos = (self.__transChunkToPxl(startingChunk) + self.__obstacleDict[startingChunk][1][0], self.__obstacleDict[startingChunk][1][1])
+                    obstacleRect = self.getVineRect(pos)
 
-
+                    #Check if the player collides
+                    if obstacleRect.colliderect(self.__plyr.getBoundingBox()):
+                        #Since this is a coin, collision is handled internally, increment coins collected...
+                        self.__plyr.coinsCollected += 1
+                        self.__obstacleDict[startingChunk] = (self.__obstacleDict[startingChunk][0], self.__obstacleDict[startingChunk][1], False)
+        
+        #Return false if no collisions were found
         return False
-
 
     def getSawBladeRect(self, pos):
         return pygame.Rect((pos[0] - self.__plyr.distance) + (6 * constants.OBSTACLES_SCALE), (pos[1] + (constants.SCREEN_HEIGHT - constants.GRASS_OFFSET)) + (6 * constants.OBSTACLES_SCALE), self.__sawBladeTexture.get_width() - 2 * (6 * constants.OBSTACLES_SCALE), self.__sawBladeTexture.get_height() - 2 * (6 * constants.OBSTACLES_SCALE))
@@ -74,27 +107,34 @@ class obstacles:
     def getCoinRect(self, pos):
         return pygame.Rect(pos[0] - self.__plyr.distance + 7, pos[1] + (constants.SCREEN_HEIGHT - constants.GRASS_OFFSET) + 7, self.__coinTexture.get_width() - 14, self.__coinTexture.get_height() - 14)
 
+    #Takes a chunk, and returns its starting pixel (x)
+    def __transChunkToPxl(self, chunk):
+        return chunk * self.__chunkWidth
 
-
-    def update(self):
-        pass
 
     def draw(self, screen):
-        for item in self.__obstaclesList:
-            if 'saw' in item[0]: 
-                pygame.draw.rect(screen, constants.COLOR_WHITE, self.getSawBladeRect(item[1]))               
-                screen.blit(self.__sawBladeTexture, (item[1][0] - self.__plyr.distance, item[1][1] + (constants.SCREEN_HEIGHT - constants.GRASS_OFFSET)))
-            elif 'vine' in item[0]:
-                pygame.draw.rect(screen, constants.COLOR_WHITE, self.getVineRect(item[1]))
-                screen.blit(self.__vineTexture, (item[1][0] - self.__plyr.distance, item[1][1]))
-            elif 'box' in item[0]:
-                pygame.draw.rect(screen, constants.COLOR_WHITE, self.getBoxRect(item[1]))
-                screen.blit(self.__boxTexture, (item[1][0] - self.__plyr.distance, item[1][1] + (constants.SCREEN_HEIGHT - constants.GRASS_OFFSET)))
-            elif 'coin' in item[0]:
-                #Only draw if it has not been picked up yet
-                if item[2]:
-                    pygame.draw.rect(screen, constants.COLOR_WHITE, self.getCoinRect(item[1]))                    
-                    screen.blit(self.__coinTexture, (item[1][0] - self.__plyr.distance, item[1][1] + (constants.SCREEN_HEIGHT - constants.GRASS_OFFSET)))
+        currentChunk = self.__startingPlayerChunk + math.floor(self.__plyr.distance / self.__chunkWidth)
 
+        #Draw the obstacles starting from the first visible chunk
+        startingChunk = currentChunk - self.__startingPlayerChunk
 
-
+        #Drawing one extra chunk on the end eliminates pop in
+        for startingChunk in range(startingChunk, startingChunk + constants.NUM_CHUNKS + 1):
+            if startingChunk in self.__obstacleDict:
+                if 'box' in self.__obstacleDict[startingChunk][0]:
+                    pos = (self.__transChunkToPxl(startingChunk) + self.__obstacleDict[startingChunk][1][0], self.__obstacleDict[startingChunk][1][1])
+                    #pygame.draw.rect(screen, constants.COLOR_WHITE, self.getBoxRect(pos))
+                    screen.blit(self.__boxTexture, (pos[0] - self.__plyr.distance, pos[1] + (constants.SCREEN_HEIGHT - constants.GRASS_OFFSET)))
+                elif 'vine' in self.__obstacleDict[startingChunk][0]:
+                    pos = (self.__transChunkToPxl(startingChunk) + self.__obstacleDict[startingChunk][1][0], self.__obstacleDict[startingChunk][1][1])
+                    #pygame.draw.rect(screen, constants.COLOR_WHITE, self.getVineRect(pos))
+                    screen.blit(self.__vineTexture, (pos[0] - self.__plyr.distance, pos[1]))
+                elif 'saw' in self.__obstacleDict[startingChunk][0]:
+                    pos = (self.__transChunkToPxl(startingChunk) + self.__obstacleDict[startingChunk][1][0], self.__obstacleDict[startingChunk][1][1])
+                    #pygame.draw.rect(screen, constants.COLOR_WHITE, self.getSawBladeRect(pos))
+                    screen.blit(self.__sawBladeTexture, (pos[0] - self.__plyr.distance, pos[1] + (constants.SCREEN_HEIGHT - constants.GRASS_OFFSET)))
+                else:
+                    if self.__obstacleDict[startingChunk][2]:
+                        pos = (self.__transChunkToPxl(startingChunk) + self.__obstacleDict[startingChunk][1][0], self.__obstacleDict[startingChunk][1][1])
+                        #pygame.draw.rect(screen, constants.COLOR_WHITE, self.getCoinRect(pos))
+                        screen.blit(self.__coinTexture, (pos[0] - self.__plyr.distance, pos[1] + (constants.SCREEN_HEIGHT - constants.GRASS_OFFSET)))
